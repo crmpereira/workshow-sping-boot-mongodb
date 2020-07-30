64f179800c7a4416256874edcda77cac6149bca3
cesar.pereira <cesar.pereiram@gmnail.com>
2020-07-30 20:20:31 -0300

    Projeção dados Author com DTO

--- src/main/java/com/cesarpereira/workshopmongo/config/Instatiation.java
+++ src/main/java/com/cesarpereira/workshopmongo/config/Instatiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.cesarpereira.workshopmongo.domain.Post;
 import com.cesarpereira.workshopmongo.domain.User;
+import com.cesarpereira.workshopmongo.dto.AuthorDTO;
 import com.cesarpereira.workshopmongo.repository.PostRepository;
 import com.cesarpereira.workshopmongo.repository.UserRepository;
 
@@ -35,13 +36,14 @@ public class Instatiation implements CommandLineRunner {
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "PartiuViagem", "Vou Viajar para Sao Paulo Abraços.", maria );
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom Dia", "Vou Acordei feliz.", maria );
-		
-		
-		postRepository.saveAll(Arrays.asList(post1,post2));
 		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "PartiuViagem", "Vou Viajar para Sao Paulo Abraços.", new AuthorDTO(maria) );
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom Dia", "Vou Acordei feliz.", new AuthorDTO(maria) );
+				
+		postRepository.saveAll(Arrays.asList(post1,post2));
+		
 		
 	}
 
--- src/main/java/com/cesarpereira/workshopmongo/domain/Post.java
+++ src/main/java/com/cesarpereira/workshopmongo/domain/Post.java
@@ -6,12 +6,12 @@ import java.util.Date;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.cesarpereira.workshopmongo.dto.AuthorDTO;
+
 
 @Document
 public class Post implements Serializable {
 
-	
-	
 	private static final long serialVersionUID = 1L;
 	
 	@Id
@@ -19,7 +19,7 @@ public class Post implements Serializable {
 	private Date date;
 	private String title;
 	private String body;
-	private User author;
+	private AuthorDTO author;
 	
 	public Post() {
 		
@@ -27,7 +27,7 @@ public class Post implements Serializable {
 	
 	
 	
-	public Post(String id, Date date, String title, String body, User author) {
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
 		this.date = date;
@@ -62,10 +62,10 @@ public class Post implements Serializable {
 	public void setBody(String body) {
 		this.body = body;
 	}
-	public User getAuthor() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
 	
