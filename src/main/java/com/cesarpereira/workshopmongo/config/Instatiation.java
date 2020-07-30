59fa3a66cb906fadb9b753b63813c81cd6f64ee8
cesar.pereira <cesar.pereiram@gmnail.com>
2020-07-30 20:29:34 -0300

    Referenciando Posts do Usuario

--- src/main/java/com/cesarpereira/workshopmongo/config/Instatiation.java
+++ src/main/java/com/cesarpereira/workshopmongo/config/Instatiation.java
@@ -41,8 +41,10 @@ public class Instatiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "PartiuViagem", "Vou Viajar para Sao Paulo Abraços.", new AuthorDTO(maria) );
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom Dia", "Vou Acordei feliz.", new AuthorDTO(maria) );
-				
 		postRepository.saveAll(Arrays.asList(post1,post2));
+	
+		maria.getPosts().addAll(Arrays.asList(post1,post2));
+		userRepository.save(maria);
 		
 		
 	}
