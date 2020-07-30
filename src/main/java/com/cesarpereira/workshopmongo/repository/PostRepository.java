fd68abfd1c4c8d794a965bd9723fc829acd134c9
cesar.pereira <cesar.pereiram@gmnail.com>
2020-07-30 20:10:06 -0300

    Criando Entity POST com User aninhado

---
+++ src/main/java/com/cesarpereira/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,15 @@
+package com.cesarpereira.workshopmongo.repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cesarpereira.workshopmongo.domain.Post;
+
+
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+	
+	
+	
+}
