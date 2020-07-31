8fa8c53677b94529a341f9840e445490aa6f7949
cesar.pereira <cesar.pereiram@gmnail.com>
2020-07-31 18:54:27 -0300

    Consulta Simples com QueryMethods

--- src/main/java/com/cesarpereira/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/cesarpereira/workshopmongo/repository/PostRepository.java
@@ -1,4 +1,6 @@
 package com.cesarpereira.workshopmongo.repository;
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,8 +10,7 @@ import com.cesarpereira.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-
-	
-	
+		
+	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 }
